2c8316da23e8ef43b824ea632adc202965b277e5
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2025-01-21 23:08:03 +0700

    update hello service

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Service\HelloService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HelloController
+#[Route('/hello', name: 'hello')]
+class HelloController extends AbstractController
 {
-    #[Route('/hello/say')]
+    private HelloService $helloService;
+
+    public function __construct(HelloService $helloService)
+    {
+        $this->helloService = $helloService;
+    }
+
+    #[Route('/say', name: 'say')]
     public function say(): Response
     {
         return new Response(
-            "Hello World!"
+            $this->helloService->getMessage()
         );
     }
 }
---
+++ src/Service/HelloService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+class HelloService
+{
+    public function getMessage(): string
+    {
+        return 'Hello World!';
+    }
+}
